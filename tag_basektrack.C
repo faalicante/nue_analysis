d32f50937611f9738db0c47000d58238eaa56652
Fabio Alicante <alicantef@gmail.com>
2024-09-03 14:42:27 +0200

    start and end as treshold

--- tag_basektrack.C
+++ tag_basektrack.C
@@ -18,42 +18,44 @@ float set_limits(TH3F &h3, TH2F &h2) {
   return lastmean;
 }
 
-float eval_bkg(TH2F &h2) { //make it two control regions next to the circle
-  int xmi = 291000;
-  int xma = 297000;
-  int ymi = 86000;
-  int yma = 93000;
-  // int xmi = 44000;
-  // int xma = 57000;
-  // int ymi = 42000;
-  // int yma = 56000;
-  // h2.GetXaxis()->SetRangeUser(xmi,xma);
-  // h2.GetYaxis()->SetRangeUser(ymi,yma);
-  int xmax = h2.GetXaxis()->FindBin(xma);
-  int xmin = h2.GetXaxis()->FindBin(xmi);
-  int ymax = h2.GetYaxis()->FindBin(yma);
-  int ymin = h2.GetYaxis()->FindBin(ymi);
-  float bkg = h2.Integral(xmin,xmax,ymin,ymax)/(xmax-xmin+1)/(ymax-ymin+1);
-  printf("Average background %f\n", bkg);
-  return bkg;
-}
+void eval_bkg(TH2F &h2, TEllipse &el, float *bkg) { //make it two control regions next to the circle
+  float x0 = el.GetX1();
+  float y0 = el.GetY1();
+  float r1 = el.GetR1();
 
-float distance(float x1, float y1, float x2, float y2) {
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-}
+  float x1 = x0 - r1 * 1.2;
+  float x2 = x0 + r1 * 1.2;
+  
+  int xmi1 = (int)(x1-r1);
+  int xma1 = (int)(x1+r1);
+  int ymi1 = (int)(y0-r1);
+  int yma1 = (int)(y0+r1);
 
-bool compareCircles(TEllipse *el, TEllipse *locel, float tolerance) {
-    float x1 = el->GetX1();
-    float y1 = el->GetY1();
-    float x2 = locel->GetX1();
-    float y2 = locel->GetY1();
-    return (distance(x1, y1, x2, y2) < tolerance);
+  int xmax1 = h2.GetXaxis()->FindBin(xma1);
+  int xmin1 = h2.GetXaxis()->FindBin(xmi1);
+  int ymax1 = h2.GetYaxis()->FindBin(yma1);
+  int ymin1 = h2.GetYaxis()->FindBin(ymi1);
+
+  int xmi2 = (int)(x2-r1);
+  int xma2 = (int)(x2+r1);
+  int ymi2 = (int)(y0-r1);
+  int yma2 = (int)(y0+r1);
+
+  int xmax2 = h2.GetXaxis()->FindBin(xma2);
+  int xmin2 = h2.GetXaxis()->FindBin(xmi2);
+  int ymax2 = h2.GetYaxis()->FindBin(yma2);
+  int ymin2 = h2.GetYaxis()->FindBin(ymi2);
+
+  float bkg1 = h2.Integral(xmin1,xmax1,ymin1,ymax1)/(xmax1-xmin1+1)/(ymax1-ymin1+1);
+  float bkg2 = h2.Integral(xmin2,xmax2,ymin2,ymax2)/(xmax2-xmin2+1)/(ymax2-ymin2+1);
+  *bkg = 0.5 * (bkg1 + bkg2);
+  // printf("Average background %f\n", *bkg);
 }
 
 void getMax(TH2F &h2, TObjArray &peaks, TObjArray &txt) {
   Int_t MaxBin = h2.GetMaximumBin();
   Int_t ix,iy,iz;
-  int radius = 250;
+  int radius = 200;
   h2.GetBinXYZ(MaxBin, ix, iy, iz);
   float x = ((TAxis*)h2.GetXaxis())->GetBinCenter(ix);
   float y = ((TAxis*)h2.GetYaxis())->GetBinCenter(iy);
@@ -85,7 +87,7 @@ void drawEllipse(TObjArray &peaks, TObjArray &txt, int col) {
   }
 }
 
-void getEntriesInEllipse(TH2F &h2, TEllipse &el, float bkg, int *signif_bins, float *entries) {
+void getEntriesInEllipse(TH2F &h2, TEllipse &el, int *signif_bins, float *entries, float *bkg) {
   int nBinsX = h2.GetNbinsX();
   int nBinsY = h2.GetNbinsY();
   float x0 = el.GetX1();
@@ -101,121 +103,90 @@ void getEntriesInEllipse(TH2F &h2, TEllipse &el, float bkg, int *signif_bins, fl
       float distance = (pow(x-x0,2)+pow(y-y0,2))/pow(r1,2);
       if (distance <= 1) {
         int binContent = h2.GetBinContent(i, j);
-        float excess = (binContent - bkg)/TMath::Sqrt(bkg);
-        if (excess > 1) {
-          *entries = *entries + binContent;
-          *signif_bins = *signif_bins + 1;
+        // float excess = (binContent - bkg)/TMath::Sqrt(bkg);
+        // if (excess > 1) {
+        *entries = *entries + binContent - *bkg;
+          // *signif_bins = *signif_bins + 1;
         }
       }
     }
-  }
-  *entries = *entries - bkg;
+  // }
+  // *entries = *entries - bkg;
   if (*entries<0) *entries = 0;
 }
 
-void count_bins(TH2F &h2, TObjArray &peaks, float bkg) {
+void count_bins(int npmax, TH2F &h2, TObjArray &peaks, int plate, TH1F *h_long[npmax]) {
   int np = peaks.GetEntries();
   int signif_bins;
-  float entries;
-
-  for(int j=0; j<np; j++) {
-    TEllipse *el = ((TEllipse*)(peaks.At(j)));
-    getEntriesInEllipse(h2, *el, bkg, &signif_bins, &entries);
-    width = TMath::Sqrt(signif_bins * bin_size * bin_size / TMath::Pi());
-    printf("Peak %i has %i significant bins and %i entries\n", j+1, signif_bins, (int)entries);
-  }
-}
-
-void count_bins(TH2F &h2, TObjArray &peaks, float bkg, int plate, TH1F *h_prof[5], TH1F *h_signif[5], TH1F *h_width[5]) {
-  int np = peaks.GetEntries();
-  int signif_bins;
+  float bkg;
   float entries;
   float width;
   float bin_size = 50./3;
 
-  for(int j=0; j<np; j++) {
-    TEllipse *el = ((TEllipse*)(peaks.At(j)));
-    getEntriesInEllipse(h2, *el, bkg, &signif_bins, &entries);
-    printf("Peak %i has %i significant bins and %i entries\n", j+1, signif_bins, (int)entries);
-    h_prof[j]->SetBinContent(plate, entries);
-    h_signif[j]->SetBinContent(plate, signif_bins);
+  for(int i=0; i<np; i++) {
+    TEllipse *el = ((TEllipse*)(peaks.At(i)));
+    eval_bkg(h2, *el, &bkg);
+    getEntriesInEllipse(h2, *el, &signif_bins, &entries, &bkg);
+    // printf("Peak %i has %i significant bins and %i entries\n", i+1, signif_bins, (int)entries);
+    // entries = entries - bkg;
+    if (entries<0) entries = 0;
+    h_long[i]->SetBinContent(plate, entries);
     width = TMath::Sqrt(signif_bins * bin_size * bin_size / TMath::Pi());
-    h_width[j]->SetBinContent(plate, width);
   }
 }
 
-void makePlots(int npmax, TH1F* h_prof[5], TH1F* h_signif[5], TH1F* h_width[5]) {
+void makePlots(int npmax, TH1F* h_long[npmax]) {
   TCanvas *c2 = new TCanvas("c2", "c2", 1000, 600);
-  TCanvas *c3 = new TCanvas("c3", "c3", 1000, 600);
-  TCanvas *c4 = new TCanvas("c4", "c4", 1000, 600);
   TLegend *leg = new TLegend(0.75, 0.7, 0.87, 0.87);
   leg->SetTextFont(53);
   leg->SetBorderSize(0);
-  int line_color[5] = {1, 2, 3, 4, 6};
-  float max_prof = 0;
-  float max_signif = 0;
-  float max_width = 0;
+  float max_long = 0;
 
-  for(int j=0; j<npmax; j++) {
-    leg->AddEntry(h_prof[j], Form("Peak %i",j), "L2");
-    c2->cd();
-    if (h_prof[j]->GetMaximum() > max_prof) {
-      max_prof = h_prof[j]->GetMaximum();
-      h_prof[0]->GetYaxis()->SetRangeUser(0, max_prof*1.2);
+  for(int i=0; i<npmax; i++) {
+    leg->AddEntry(h_long[i], Form("Peak %i",i), "L2");
+    h_long[i]->SetLineColor(i+1);
+    h_long[i]->SetLineWidth(2);
+    h_long[i]->Scale(1./h_long[i]->Integral());
+    if (h_long[i]->GetMaximum() > max_long) {
+      max_long = h_long[i]->GetMaximum();
+      h_long[0]->GetYaxis()->SetRangeUser(0, max_long*1.2);
     }
-    h_prof[j]->SetLineColor(line_color[j]);
-    h_prof[j]->SetLineWidth(2);
-    h_prof[j]->Draw("same");
-    c3->cd();
-    if (h_signif[j]->GetMaximum() > max_signif) {
-      max_signif = h_signif[j]->GetMaximum();
-      h_signif[0]->GetYaxis()->SetRangeUser(0, max_signif*1.2);
-    }
-    h_signif[j]->SetLineColor(line_color[j]);
-    h_signif[j]->SetLineWidth(2);
-    h_signif[j]->Draw("same");
-    c4->cd();
-    if (h_width[j]->GetMaximum() > max_width) {
-      max_width = h_width[j]->GetMaximum();
-      h_width[0]->GetYaxis()->SetRangeUser(0, max_width*1.2);
-    }
-    h_width[j]->SetLineColor(line_color[j]);
-    h_width[j]->SetLineWidth(2);
-    h_width[j]->Draw("same");
+    h_long[i]->Draw("same&&hist");
     c2->Update();
-    c3->Update();
-    c4->Update();
+
   }
-  c2->Print("prof.png");
-  c3->Print("signif.png");
-  c4->Print("width.png");
+  c2->Print("longitudinal_xz.png");
 }
 
-void makeNtuple(TH2F &h2, TObjArray &peaks, float bkg) {
+void findStart(TH1F* h_long, int *firstPlate, int *lastPlate) {
+ *firstPlate = h_long->FindFirstBinAbove(0.005);
+ *lastPlate = h_long->FindLastBinAbove(0.01);
+}
+
+void makeNtuple(int np, TH1F *h_long[np], TObjArray &peaks) {
   TFile *output = new TFile("peaks.root","RECREATE");
-  TNtuple *ntuple = new TNtuple("showers","tagged showers","x:y:first:last:width:entries")
+  TNtuple *ntuple = new TNtuple("showers","tagged showers","x:y:start:end");
+  int signif_bins;
   
   float entries;
   float width;
-  float bin_size = 50./3;
-  int np = peaks.GetEntries();
+  float bin_size = 50;
+  int firstPlate, lastPlate;
+
   for(int i=0; i<np; i++) {
     TEllipse *el = ((TEllipse*)(peaks.At(i)));
     float x = el->GetX1();
     float y = el->GetY1();
-
-    getEntriesInEllipse(h2, *el, bkg, &signif_bins, &entries);
-    width = TMath::Sqrt(signif_bins * bin_size * bin_size / TMath::Pi());
-    printf("Peak %i has %i significant bins and %i entries\n", j+1, signif_bins, (int)entries);
-    ntuple->Fill(x, y, width, entries);
+    findStart(h_long[i], &firstPlate, &lastPlate);
+    ntuple->Fill(x, y, firstPlate, lastPlate);
   }
- 
+
   output->Write();
   output->Close();
 }
 
 void tag_basektrack() {
-  int ntag = 20;
+  int ntag = 5;
   TH2F *h2 = (TH2F *)(gDirectory->Get("XYseg"));
   TH3F *h3 = (TH3F *)(gDirectory->Get("XYPseg"));
   h2->Smooth();
@@ -233,58 +204,26 @@ void tag_basektrack() {
   drawEllipse(peaks,txt, kBlack);
   c->Update();
   c->Print("sh.gif+180");
-  // count_bins(*h2, peaks, bkg);
   
-  const int nplates = 60; //change nplates for mc
-  for(int i=1; i<=nplates; i++) { 
-    h3->GetZaxis()->SetRange(i,i);
-    TH2F *h = (TH2F*)(h3->Project3D("yx"));
-    h->SetTitle(Form("Plate %d",i));
-    h->Smooth();
-    //h->Rebin2D(3,3);
-    bkg = eval_bkg(*h);
+  TH1F *h_long[ntag];
+  for(int i=0; i<ntag; i++) {
+    h_long[i] = new TH1F(Form("h_long_%i", i+1),"Longitudinal entries;plate", 60, 1, 61);
+  }
 
+  const int nplates = 60; //change nplates for mc
+  for(int p=1; p<=nplates; p++) { 
+    h3->GetZaxis()->SetRange(p,p);
+    TH2F *h = (TH2F*)(h3->Project3D("yx"));
+    h->SetTitle(Form("Plate %d",p));
     h->Draw("colz");
 
     drawEllipse(peaks,txt, kBlack);
-
-    TObjArray locpeaks;
-    TObjArray loctxt;
-    get_peaks(*h,locpeaks,loctxt,ntag);
-    drawEllipse(locpeaks,loctxt, kWhite);
-
+    count_bins(ntag, *h, peaks, p, &h_long[0]);
     c->Update();
     c->Print("sh.gif+12");
   }
   c->Print("sh.gif++");
-  makeNtuple();
-}
 
-
-
-int main(TObjArray &peaks, TObjArray &locpeaks, int radius) {
-  float tolerance = radius/2;
-
-  // Arrays to store the start and end matching plate indices for each circle
-  int firstTag[ntag];
-  int lastTag[ntag];
-  // Initialize the start and end indices with -1 (indicating no match found yet)
-  for (int i = 0; i < ntag; i++) {
-      firstTag[i] = -1;
-      lastTag[i] = -1;
-  }
-
-  for (int i = 0; i < ntag; i++) {
-    TEllipse *el = ((TEllipse*)(peaks.At(i)));
-    for (int j = 0; j < ntag; j++) {
-      TEllipse *locel = ((TEllipse*)(locpeaks.At(j)));
-      if (compareCircles(el[i], locel[j], tolerance)) {
-        if (firstTag[i] == -1) {
-          firstTag[i] = plate;  // Set the first match plate for this circle
-        }
-        lastTag[i] = plate;  // Update the last match plate for this circle
-        break; // Move to the next circle in the true set once a match is found
-      }
-    }
-  }
+  makePlots(ntag, &h_long[0]);
+  makeNtuple(ntag, &h_long[0], peaks);
 }
