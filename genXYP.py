4bc39be424f1777aa29cec4dc155d43f37404a87
Fabio Alicante <alicantef@gmail.com>
2024-09-12 15:06:29 +0200

    gen with fragment

--- genXYP.py
+++ genXYP.py
@@ -1,57 +1,67 @@
 import ROOT
 import fedrarootlogon
+import os.path
+
 from argparse import ArgumentParser
 parser = ArgumentParser()
-parser.add_argument("--cell", dest="cellID", required=False, default=None, type=int)
+parser.add_argument("-f", dest="fragment", required=False, default=None, type=int)
 options = parser.parse_args()
 
-brick = 23
+brick = 121
 nx = 18
 ny = nx
-xmin = 5000.
-xmax = 185000.
-ymin = 5000.
-ymax = 185000.
-emulsioncell = ROOT.EdbCell2()
-emulsioncell.InitCell(nx,xmin,xmax,ny,ymin,ymax,1)
-icell = options.cellID
-ix=((icell % nx)) + 1
-iy=((icell // ny)) + 1
-xmincell = emulsioncell.X(ix-1)-emulsioncell.Xbin()
-xmaxcell = emulsioncell.X(ix-1)+emulsioncell.Xbin()
-ymincell = emulsioncell.Y(iy-1)-emulsioncell.Ybin()
-ymaxcell = emulsioncell.Y(iy-1)+emulsioncell.Ybin()
+xmin = 5000
+xmax = 185000
+ymin = 5000
+ymax = 185000
+# emulsioncell = ROOT.EdbCell2()
+# emulsioncell.InitCell(nx,xmin,xmax,ny,ymin,ymax,1)
+fragment = options.fragment
+# ix=(icell % nx)
+# iy=(icell // ny)
+# xmincell = emulsioncell.X(ix)-emulsioncell.Xbin()
+# xmaxcell = emulsioncell.X(ix)+emulsioncell.Xbin()
+# ymincell = emulsioncell.Y(iy)-emulsioncell.Ybin()
+# ymaxcell = emulsioncell.Y(iy)+emulsioncell.Ybin()
 bin_size = 50
-xbin = int((xmaxcell-xmincell)/bin_size)
-ybin = int((ymaxcell-ymincell)/bin_size)
-overlap_fraction = 0.25
+xbin = int((xmax-xmin)/bin_size)
+ybin = int((ymax-ymin)/bin_size)
+# overlap_fraction = 0.25
 
-rootfile = ROOT.TFile(f"hist_XYP_{ix}_{iy}.root","RECREATE")
-hXY = ROOT.TH2D(f"XYseg",f"XYseg;x[#mum];y[#mum]", xbin, xmincell, xmaxcell, ybin, ymincell, ymaxcell)
-hXYP = ROOT.TH3D(f"XYPseg",f"XYPseg;x[#mum];y[#mum];p", xbin, xmincell, xmaxcell, ybin, ymincell, ymaxcell,60,1,60)
+hXYs = {}
+rootfile = ROOT.TFile(f"hist_XYP_run1b121_{fragment}.root","RECREATE")
+hXY = ROOT.TH2D(f"XYseg",f"XYseg;x[#mum];y[#mum]", xbin, xmin, xmax, ybin, ymin, ymax)
 hTXTY = ROOT.TH2F("TXTYseg", "TXTYseg", 200, -0.1, 0.1, 200, -0.1, 0.1)
+for p in range(1, 58):
+  hXYs[p] = ROOT.TH2D(f"XYseg_{p}",f"XYPseg_{p};x[#mum];y[#mum]", xbin, xmin, xmax, ybin, ymin, ymax)
 
 path = f"/eos/experiment/sndlhc/emulsionData/2022/emureco_Napoli/RUN1/b{brick:06}"
 #scanset
-sspath = path+f"/trackfiles/57_1/cell_{ix}_{iy}"                   ##CREATE FOLDERS
+sspath = path+f"/.."                  
 sproc = ROOT.EdbScanProc()
 sproc.eProcDirClient=sspath
-id = ROOT.EdbID(brick,0,ix,iy)
+id = ROOT.EdbID(brick,100,0,0)
 ss = sproc.ReadScanSet(id)
 ss.Brick().SetID(brick)
 
 npl = ss.eIDS.GetEntries()
 cutstring = f"eCHI2P<2.4&&s.eW>20&&eN1<=1&&eN2<=1&&s1.eFlag>=0&&s2.eFlag>=0"
-cutstring = cutstring + "&&" + f"TMath::Abs(s.eX-{emulsioncell.X(ix-1)}) < {emulsioncell.Xbin()*(1.+overlap_fraction)/2} && TMath::Abs(s.eY-{emulsioncell.Y(iy-1)}) < {emulsioncell.Ybin()*(1.+overlap_fraction)/2}"
+# cutstring = cutstring + "&&" + f"TMath::Abs(s.eX-{emulsioncell.X(ix)}) < {emulsioncell.Xbin()*(1.+overlap_fraction)/2} && TMath::Abs(s.eY-{emulsioncell.Y(iy)}) < {emulsioncell.Ybin()*(1.+overlap_fraction)/2}"
 cut = ROOT.TCut(cutstring)
 cut.Print()
 for i in range(npl):
   idplate = ss.GetID(i)
   nplate = idplate.ePlate
   plate = ss.GetPlate(idplate.ePlate)
+
+  cp_file = path+f"/p{nplate:03}/{brick}.{nplate}.0.0.{fragment}.cp.root"
+  if not os.path.isfile(cp_file): 
+    print(f"Missin file /p{nplate:03}/{brick}.{nplate}.0.0.{fragment}.cp.root")
+    continue
+
   p = ROOT.EdbPattern()
   ect = ROOT.EdbCouplesTree()
-  ect.InitCouplesTree("couples",path+f"/p{nplate:03}/{brick}.{nplate}.{ix}.{iy}.cp.root","READ")
+  ect.InitCouplesTree("couples",cp_file,"READ")
   #addingcut
   ect.eCut = cut 
   cutlist = ect.InitCutList()
@@ -78,14 +88,15 @@ for i in range(npl):
     seg.SetTX(tx)
     seg.SetTY(ty)
     hXY.Fill(seg.X(), seg.Y())
-    hXYP.Fill(seg.X(), seg.Y(),nplate)
     hTXTY.Fill(seg.TX(), seg.TY())
+    hXYs[nplate].Fill(seg.X(), seg.Y())
 ect.Close()
 
 rootfile.cd()
 hXY.Write()
-hXYP.Write()
 hTXTY.Write()
+for p in range(1,58):
+  hXYs[p].Write()
 rootfile.Close()
 
 
