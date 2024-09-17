247f9aa218c9026e9c18093682f6a692286cb036
Fabio Alicante <alicantef@gmail.com>
2024-09-17 12:33:52 +0200

    spherocity with fragments

--- spherocity.py
+++ spherocity.py
@@ -1,3 +1,5 @@
+##IMPORTANT###
+##NEED TO SAVE THE NTUPLE PER PEAK TO GET SPHEROCITY EVENT
 import ROOT
 import fedrarootlogon
 from math import pi
@@ -8,8 +10,10 @@ import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("-b",dest="brick", type=int, required=True)
+parser.add_argument("-f",dest="fragment", type=int, required=True)
 options = parser.parse_args()
 brick = options.brick
+fragment = options.fragment
 
 # Function to minimise
 def cross(angle, vecs):
@@ -26,99 +30,119 @@ def spherocity(vecs, initial_angle = 0.):
     return np.pi**2/4.*(num/denom)**2, direction
 
 vecs = {}
-def append_list_to_dict(key, values_list):
+def append_list_to_vecs(key, values_list):
     if key in vecs:
         vecs[key].append(values_list)
     else:
         vecs[key] = [values_list]
 
-xmin_muon = 289000 - 2000
-xmax_muon = 299000 + 2000
-ymin_muon = 84000 - 2000
-ymax_muon = 94000 + 2000
-bin_size = 50
-xbin = int((xmax_muon-xmin_muon)/bin_size)
-ybin = int((ymax_muon-ymin_muon)/bin_size)
+def isInCluster(peak, seg):
+  x = seg.X()
+  y = seg.Y()
+  x0 = peak.x
+  y0 = peak.y
+  r0 = 250
+  distance = ((x-x0)**2 + (y-y0)**2) / r0**2
+  return distance <= 1
 
 
-prepath = f'/eos/user/f/falicant/Simulations_sndlhc/muon1E5_simsndlhc'
-rootfile = ROOT.TFile(prepath+f"/sh_theta_{brick}.root","RECREATE")
-ntuple = ROOT.TNtuple("couples", "Tree of couples","p:x:y:tx:ty:theta:scatt")
+# path = f'/eos/user/f/falicant/Simulations_sndlhc/muon1E5_simsndlhc/b{brick:06}'
+# path = f'/eos/user/f/falicant/Simulations_sndlhc/nuecc_withcrisfiles_25_July_2022/b{brick:06}'
+path = '/eos/user/f/falicant/nue_search/R1B121/30'
+path = '/afs/cern.ch/work/f/falicant/public/nue_analysis/30'
+datapath = '/eos/experiment/sndlhc/emulsionData/2022/emureco_Napoli/RUN1/b000121'
 
-shower_file = ROOT.TFile.Open(prepath+"/muon_showers.root")
-cbmsim = shower_file.cbmsim
-shower_list = {}
+shower_file = ROOT.TFile.Open(path+f"/peaks_{fragment}.root")
+peaks = shower_file.showers
 h = {}
-for event in cbmsim:
-  if event.nHits < 5000: continue
-  shower_list[event.event] = event.brick  
-  h[f'xy_{event.event}'] = ROOT.TH2D(f'xy_{event.event}', f'xy_{event.event}; x [#mum]; y [#mum]', xbin, xmin_muon, xmax_muon, ybin, ymin_muon, ymax_muon)
-  h[f'theta_{event.event}'] = ROOT.TH2D(f'theta_{event.event}', f'theta_{event.event}; plate; theta [#mrad]', 60, 1, 61, 3140, 0, pi*100)
-h['xy'] = ROOT.TH2D('xy', 'xy; x [#mum]; y [#mum]', xbin, xmin_muon, xmax_muon, ybin, ymin_muon, ymax_muon)
-h['theta_mean'] = ROOT.TH1D('theta_mean', 'theta_mean; theta [#mrad]', 3140, 0, pi*100)
-h['theta_std'] = ROOT.TH1D('theta_std', 'theta_std; theta [#mrad]', 3140, 0, pi*100)
-h['spherocity'] = ROOT.TH1D('spherocity', 'spherocity', 100, 0, 1)
-h['direction'] = ROOT.TH1D('direction', 'direction; phi', 2*3140, 0, 2*pi)
-print('Selected muon shower events:', len(shower_list))
+
+#scanset
+sspath = datapath+f"/.."
+sproc = ROOT.EdbScanProc()
+sproc.eProcDirClient=sspath
+id = ROOT.EdbID(brick,100,0,0)
+ss = sproc.ReadScanSet(id)
+ss.Brick().SetID(brick)
+npl = ss.eIDS.GetEntries()
 
 cutstring = f"eCHI2P<2.4&&s.eW>20&&eN1<=1&&eN2<=1&&s1.eFlag>=0&&s2.eFlag>=0"
 cut = ROOT.TCut(cutstring)
-# cut_muon.Print()
+# cut.Print()
 
-brick_path = prepath +f'/b{brick:06}'
-for plate in range(1,61):
-  ect = ROOT.EdbCouplesTree()
-  ect.InitCouplesTree("couples",brick_path+f"/p{plate:03}/{brick}.{plate}.0.0.cp.root","READ")
-  #addingcut
-  ect.eCut = cut
-  cutlist = ect.InitCutList()
-  if not ect.eTree: continue #no TTree in file
-  if not cutlist:
-    print("We have no entries, quitting!")
-    continue
-  nsegcut = cutlist.GetN()
-  print(f"Processing brick {brick} plate {plate}")
-  for ientry in range(nsegcut):
-    iseg = cutlist.GetEntry(ientry)
-    ect.GetEntry(iseg)
-    seg=ect.eS
-    event = seg.MCEvt()
-    if event not in shower_list: continue
-    if shower_list[event] != brick: continue
-    sx = seg.X()
-    sy = seg.Y()
-    stx = seg.TX()
-    sty = seg.TY()
-    append_list_to_dict(event, [stx, sty])
-    stheta =seg.Theta()*100
-    ntuple.Fill(plate, sx, sy, stx, sty , stheta)
-    h[f'xy_{event}'].Fill(sx,sy)
-    h[f'theta_{event}'].Fill(plate, stheta)
+rootfile = ROOT.TFile(path+f"/sh_angle_{fragment}.root","RECREATE")
+ntuple = ROOT.TNtuple("couples", "Tree of couples","fragment:peak:max:maxpeak:start:end:theta:spherocity")
+h['theta_mean'] = ROOT.TH1D('theta_mean', 'theta_mean; theta [#mrad]', 150, 0, 150)
+h['spherocity'] = ROOT.TH1D('spherocity', 'spherocity', 100, 0, 1)
+h['direction'] = ROOT.TH1D('direction', 'direction; phi', 150, 0, 150)
 
-ect.Close()
-rootfile.cd()
-for event in vecs:
-  if len(vecs[event])<100:
-    vecs.pop[event]
-    continue
-  h[f'xy_{event}'].Write()
-  h[f'theta_{event}'].Write()
-  mean = h[f'theta_{event}'].GetMean(2)
-  mean_err = h[f'theta_{event}'].GetMeanError(2)
-  std = h[f'theta_{event}'].GetStdDev(2)
-  std_err = h[f'theta_{event}'].GetStdDevError(2)
+for ipeak, peak in enumerate(peaks):
+  vecs = []
+  #if ipeak>4: break
+  # xmin = peak.x - 500
+  # xmax = peak.x + 500
+  # ymin = peak.y - 500
+  # ymax = peak.y + 500
+  # bin_size = 50
+  # xbin = int((xmax-xmin)/bin_size)
+  # ybin = int((ymax-ymin)/bin_size)
+  # h[f'xy_{ipeak}'] = ROOT.TH2D(f'xy_{ipeak}', f'xy_{ipeak}; x [#mum]; y [#mum]', xbin, xmin, xmax, ybin, ymin, ymax)
+  h[f'theta_{ipeak}'] = ROOT.TH1D(f'theta_{ipeak}', f'theta_{ipeak}; theta [#mrad]',150, 0, 150)
+
+  startingPlate = peak.start
+  if startingPlate < 5: continue
+  endingPlate = peak.end
+  for i in range(npl):
+    idplate = ss.GetID(i)
+    plate = idplate.ePlate
+    nplate = ss.GetPlate(plate)
+  # for plate in range(1,61):
+    if plate < startingPlate or plate > endingPlate: continue
+    ect = ROOT.EdbCouplesTree()
+    ect.InitCouplesTree("couples",datapath+f"/p{plate:03}/{brick}.{plate}.0.0.{fragment}.cp.root","READ")
+    ect.eCut = cut
+    cutlist = ect.InitCutList()
+    if not ect.eTree: continue
+    if not cutlist:
+      print("We have no entries, quitting!")
+      continue
+    nsegcut = cutlist.GetN()
+    print(f"Processing brick {brick} peak {ipeak} plate {plate}")
+    for ientry in range(nsegcut):
+      iseg = cutlist.GetEntry(ientry)
+      ect.GetEntry(iseg)
+      seg=ect.eS
+#      event = seg.MCEvt()
+      seg.SetZ(nplate.Z())
+      seg.SetPID(i)
+      seg.Transform(nplate.GetAffineXY())
+
+      if isInCluster(peak, seg):
+        # sx = seg.X()
+        # sy = seg.Y()
+        stx = seg.TX()
+        sty = seg.TY()
+        # append_list_to_vecs(ipeak, [stx, sty])
+        vecs.append([stx, sty])
+        stheta =seg.Theta()*1000
+        # ntuple.Fill(brick, ipeak, plate, sx, sy, stx, sty , stheta)
+        # h[f'xy_{ipeak}'].Fill(sx,sy)
+        h[f'theta_{ipeak}'].Fill(stheta)
+    ect.Close()
+  sph, direction = spherocity(vecs)
+  mean = h[f'theta_{ipeak}'].GetMean(2)
   h['theta_mean'].Fill(mean)
-  h['theta_std'].Fill(std)
-  sph, direction = spherocity(vecs[event])
   h['spherocity'].Fill(sph)
   h['direction'].Fill(direction[0])
+  ntuple.Fill(fragment, ipeak, peak.peak, peak.maxplate, startingPlate, endingPlate, mean, sph)
+
+rootfile.cd()
+# for event in vecs:
+  # h[f'xy_{event}'].Write()
+  # h[f'theta_{event}'].Write()
+  # sph, direction = spherocity(vecs[event])
+  # ntuple.Fill(fragment,event,mean,sph)
 h['theta_mean'].Write()
-h['theta_std'].Write()
 h['spherocity'].Write()
 h['direction'].Write()
 ntuple.Write()
 rootfile.Close()
-print(len(vecs), "event in this brick out of", len(shower_list)) 
-
-##issue: same event in multiple bricks
-## do it by cluster tag
