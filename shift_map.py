0218d6a3c0ac63e6d67c81a5fefdcba8bae5ef85
Fabio <alicantef@gmail.com>
2024-10-16 10:46:29 +0200

    test on 4 events

--- shift_map.py
+++ shift_map.py
@@ -4,52 +4,79 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 from skimage.feature import peak_local_max
 
+
 ROOT.gStyle.SetOptStat("e")
 
-def makeMap(hmap, tag):
-    smoothed_hist = gaussian_filter(hmap, sigma=0.5, radius=1)
-    peaks = peak_local_max(smoothed_hist, min_distance=2, threshold_abs=100)
-
+def makeTMap(hTmap):
+    smoothed_hist = gaussian_filter(hTmap, sigma=0.6)
+    peaks = peak_local_max(hTmap, min_distance=2, threshold_abs=150)
+    peak_x_coords = txedges[peaks[:, 0]] 
+    peak_y_coords = tyedges[peaks[:, 1]] 
     plt.figure(figsize=(8, 6))
-    plt.imshow(smoothed_hist.T, origin='lower',cmap='viridis', interpolation='nearest')
-    plt.plot(peaks[:, 0], peaks[:, 1], 'r.', markersize=10, label='Peaks')
+    plt.imshow(smoothed_hist.T, origin='lower',extent=[-shiftRange, shiftRange+shiftStep, -shiftRange, shiftRange+shiftStep],cmap='viridis', interpolation='nearest')
+    plt.plot(peak_x_coords+ shiftStep/2, peak_y_coords+ shiftStep/2, 'r.', markersize=10, label='Peaks')
     plt.colorbar(label='Counts')
     plt.title('2D Histogram with Detected Peaks')
     plt.xlabel('TX')
     plt.ylabel('TY')
     plt.legend()
-    plt.savefig(f'/Users/fabioali/Desktop/shift_map_{tag}.png')
+    plt.savefig(f'/Users/fabioali/Desktop/shift_Tmap_test.png')
 
-    return peaks
+    return peak_x_coords, peak_y_coords
+
+def makeMap(hmap):
+    smoothed_hist = gaussian_filter(hmap, sigma=1)
+    peaks = peak_local_max(smoothed_hist, min_distance=2, threshold_abs=150)
+    peak_x_coords = xedges[peaks[:, 0]]
+    peak_y_coords = yedges[peaks[:, 1]] 
+    plt.figure(figsize=(8, 6))
+    plt.imshow(smoothed_hist.T, origin='lower',extent=[72000,82000,86000,96000],cmap='viridis', interpolation='nearest')
+    plt.plot(peak_x_coords+50, peak_y_coords+50, 'r.', markersize=10, label='Peaks')
+    plt.colorbar(label='Counts')
+    plt.title('2D Histogram with Detected Peaks')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+    plt.savefig(f'/Users/fabioali/Desktop/shift_map_test.png')
+
+    return peak_x_coords, peak_y_coords
 
 
 # Parameters
-shiftRange = 50  #mrad
+shiftRange = 30  #mrad
 shiftStep  = 2   #mrad
 nbins      = int((shiftRange * 2)/shiftStep) + 1 
-nTag = 20
+nTag = 1
 
 # File
-path = '/Users/fabioali/cernbox/peaks_shift_1000.root'
+path = '/Users/fabioali/cernbox/peaks_shift.root'
 file = ROOT.TFile.Open(path)
 showers = file.Get("showers")
 
-x = np.zeros(shiftRange+1)
-y = np.zeros(shiftRange+1)
+tx = np.zeros(nbins)
+ty = np.zeros(nbins)
+x = np.zeros(100)
+y = np.zeros(100)
 
-hmap = {}
 
-for tag in range(1, nTag+1):
-    hmap[tag], xedges, yedges = np.histogram2d(x, y, nbins, [[-shiftRange, shiftRange+2], [-shiftRange, shiftRange+2]])
+hTmap, txedges, tyedges = np.histogram2d(tx, ty, nbins, range=[[-shiftRange, shiftRange+shiftStep], [-shiftRange, shiftRange+shiftStep]])
+hmap, xedges, yedges = np.histogram2d(x, y, 100, range=[[72000,82000], [86000,96000]])
 
 for entry in showers:
-    combination = int(entry.combination) - 1 
+    combination = int(entry.comb) - 1 
     peak = int(entry.rankbin)
-    tag = showers.tag
-    ix = combination // 31 
-    iy = combination % 31 
-    hmap[tag][ix, iy] = peak
+    x = (int(entry.x) - 72000) //100
+    y = (int(entry.y) - 86000) //100
+    ix = (combination // nbins) - shiftRange
+    iy = (combination % nbins) - shiftRange
+    contentT = hTmap[ix + shiftRange, iy + shiftRange]
+    if peak > contentT:
+        hTmap[ix + shiftRange, iy + shiftRange] = peak
+    content = hmap[x,y]
+    if peak > content:
+        hmap[x,y] = peak
 
-for tag in range(1, nTag+1):
-    peaks = makeMap(hmap[tag], tag)
-    print(peaks)
+peaksT = makeTMap(hTmap)
+peaks = makeMap(hmap)
+print(peaksT)
+print(peaks)
