fbcbb53198fcda639046e48e58ffaa92f58fd333
Fabio <alicantef@gmail.com>
2024-10-31 14:41:13 +0100

    changed paths, no save gif

--- tag/tag_shift.C
+++ tag/tag_shift.C
@@ -11,19 +11,19 @@
 int nPlates;
 TString path;
 
-void setRange(int data, TString* path, int fragment, int* nPlates) {
+void setPath(int data, TString* path, int cell, int* nPlates) {
     if (data==0) {
-        *path = TString::Format("/Users/fabioali/cernbox/shift_muon/%i", fragment);
-        // *path = TString::Format("/eos/user/f/falicant/Simulations_sndlhc/muon1E5_simsndlhc/b%05i", fragment);
+        // *path = TString::Format("/Users/fabioali/cernbox/shift_muon/%i", cell);
+        *path = TString::Format("/eos/experiment/sndlhc/users/falicant/shift_muon/%i", cell);
         *nPlates = 60;
     }
     else if (data==1) {
-        *path = TString::Format("/eos/user/f/falicant/Simulations_sndlhc/nuecc_withcrisfiles_25_July_2022/b%05i", fragment);
+        *path = TString::Format("/eos/user/f/falicant/Simulations_sndlhc/nuecc_withcrisfiles_25_July_2022/b%05i", cell);
         *nPlates = 60;
     }
     else if (data==2) {
-        // *path = "/Users/fabioali/cernbox";
-        *path = TString::Format("/eos/user/f/falicant/nue_search/R1B121/gen1/hist/shift/%i", fragment);
+        // *path = TString::Format("/Users/fabioali/cernbox/shift/%i", cell);
+        *path = TString::Format("/eos/experiment/sndlhc/users/falicant/RUN1/b121/shift/%i", cell);
         *nPlates = 57;
     }
 }
@@ -155,8 +155,9 @@ void count_bins(int npmax, TH2F &h2, TObjArray &peaks, int plate, TH1F **h_long,
 
   for(int i=0; i<np; i++) {
     TEllipse *el = ((TEllipse*)(peaks.At(i)));
-    if (data==0) eval_bkg(h2, &bkg);
-    else if (data==2) eval_bkg(h2, *el, &bkg);
+    eval_bkg(h2, *el, &bkg);
+    // if (data==0) eval_bkg(h2, &bkg);
+    // else if (data==2) eval_bkg(h2, *el, &bkg);
     getEntriesInEllipse(h2, *el, &signif_bins, &entries, &bkg);
     if (entries<0) entries = 0;
     h_long[i]->SetBinContent(plate, entries);
@@ -164,17 +165,17 @@ void count_bins(int npmax, TH2F &h2, TObjArray &peaks, int plate, TH1F **h_long,
 }
 
 void makePlots(int combination, TCanvas *c, int np, int npmax, TH1F *h_long, int *maxPeak, int *maxPlate) {
-  int idx = (np%3) +1;
-  if (idx==1) c->Clear("D");
-  c->cd(idx)->SetGrid(1,0);
+  // int idx = (np%3) +1;
+  // if (idx==1) c->Clear("D");
+  // c->cd(idx)->SetGrid(1,0);
   *maxPeak = h_long->GetMaximum(),
   *maxPlate = h_long->GetMaximumBin();
-  h_long->SetLineColor(1);
-  h_long->SetLineWidth(2);
-  h_long->Draw("hist");
-  if(np == 2)                 c->Print(Form("%s/longitudinal_xz_%i.pdf(", path.Data(), combination), "pdf");
-  else if(np == npmax-1)      c->Print(Form("%s/longitudinal_xz_%i.pdf)", path.Data(), combination), "pdf");
-  else if(idx == 3 && np >2 ) c->Print(Form("%s/longitudinal_xz_%i.pdf", path.Data(), combination), "pdf");
+  // h_long->SetLineColor(1);
+  // h_long->SetLineWidth(2);
+  // h_long->Draw("hist");
+  // if(np == 2)                 c->Print(Form("%s/tag/longitudinal_xz_%i.pdf(", path.Data(), combination), "pdf");
+  // else if(np == npmax-1)      c->Print(Form("%s/tag/longitudinal_xz_%i.pdf)", path.Data(), combination), "pdf");
+  // else if(idx == 3 && np >2 ) c->Print(Form("%s/tag/longitudinal_xz_%i.pdf", path.Data(), combination), "pdf");
 }
 
 void findStart(TH1F* h_long, int *firstPlate, int *lastPlate) { //add second axis?
@@ -184,12 +185,12 @@ void findStart(TH1F* h_long, int *firstPlate, int *lastPlate) { //add second axi
 }
 
 void makeNtuple(int combination, int np, TH1F **h_long, TObjArray &peaks, int *ranks) {
-  TString outputFileName = TString::Format("%s/peaks_shift_%d.root", path.Data(), combination);
+  TString outputFileName = TString::Format("%s/tag/peaks_shift_%d.root", path.Data(), combination);
   TFile *outputFile = new TFile(outputFileName, "RECREATE");
-  TFile *output = new TFile(Form("%s/peaks_shift_%i.root", path.Data(), combination),"RECREATE");
+  // TFile *output = new TFile(Form("%s/peaks_shift_%i.root", path.Data(), combination),"RECREATE");
   TNtuple *ntuple = new TNtuple("showers","tagged showers","combination:tag:x:y:start:end:peak:maxplate:nseg:rankbin");
-  TCanvas *c2 = new TCanvas("c2", "c2", 1500, 1500);
-  c2->Divide(1,3);
+  // TCanvas *c2 = new TCanvas("c2", "c2", 1500, 1500);
+  // c2->Divide(1,3);
   
   float entries;
   float width;
@@ -205,35 +206,36 @@ void makeNtuple(int combination, int np, TH1F **h_long, TObjArray &peaks, int *r
     ntuple->Fill(combination, i+1, x, y, firstPlate, lastPlate, maxPeak, maxPlate, nseg, ranks[i]);
   }
 
-  output->Write();
-  output->Close();
+  outputFile->Write();
+  outputFile->Close();
 }
 
 int main(int argc, char* argv[]) {
   if (argc != 4) {
-      std::cerr << "Usage: " << argv[0] << " <data>" << argv[1] << " <combination>" << argv[2] << " <fragment>" << argv[3] << std::endl;
+      std::cerr << "Usage: " << argv[0] << " <data>" << argv[1] << " <combination>" << argv[2] << " <cell>" << argv[3] << std::endl;
       // data = {0: muon, 1: nue, 2: data}
-      // fragment for MC is brick
+      // cell for MC is brick
       return 1;
   }
   int data = std::atoi(argv[1]);
   int combination = std::atoi(argv[2]);
-  int fragment = std::atoi(argv[3]);
-
-  setRange(data, &path, fragment, &nPlates);
+  int cell = std::atoi(argv[3]);
+  std::cout << "Combination " << combination << std::endl;
+  
+  setPath(data, &path, cell, &nPlates);
 
   TString file =  TString::Format("%s/histo_shifts_%i.root", path.Data(), combination);
   TFile* f = TFile::Open(file);
 
-  int ntag = 50;
+  int ntag = 10;
   TH2F *h2 = (TH2F *)(f->Get("XYseg"));
   h2->Smooth();
   TCanvas *c = new TCanvas("c", "c", 800, 800);
-  c->SetGrid();
+  // c->SetGrid();
   gStyle->SetOptStat(0);
   h2->Draw("colz");
   c->Update();
-  c->Print(Form("%s/sh_%i.gif+180", path.Data(), combination));
+  // c->Print(Form("%s/tag/sh_%i.gif+180", path.Data(), combination));
   
   TObjArray peaks;
   TObjArray txt;
@@ -241,7 +243,7 @@ int main(int argc, char* argv[]) {
   get_peaks(*h2,peaks,txt,ntag,ranks);
   drawEllipse(peaks,txt, kBlack);
   c->Update();
-  c->Print(Form("%s/sh_%i.gif+180", path.Data(), combination));
+  // c->Print(Form("%s/tag/sh_%i.gif+180", path.Data(), combination));
 
   
   TH1F *h_long[ntag];
@@ -250,7 +252,7 @@ int main(int argc, char* argv[]) {
   }
 
   for(int p=1; p<=nPlates; p++) { 
-    printf("Plate %i\n", p);
+    // printf("Plate %i\n", p);
     TH2F *h = (TH2F *)(f->Get(Form("XYPseg_%i",p)));
     h->Smooth();
     h->SetTitle(Form("Plate %i",p));
@@ -259,9 +261,9 @@ int main(int argc, char* argv[]) {
     drawEllipse(peaks,txt, kBlack);
     count_bins(ntag, *h, peaks, p, &h_long[0], data);
     c->Update();
-    c->Print(Form("%s/sh_%i.gif+12", path.Data(), combination));
+    // c->Print(Form("%s/tag/sh_%i.gif+12", path.Data(), combination));
   }
-  c->Print(Form("%s/sh_%i.gif++", path.Data(), combination));
+  // c->Print(Form("%s/tag/sh_%i.gif++", path.Data(), combination));
 
   makeNtuple(combination, ntag, &h_long[0], peaks,ranks);
 
