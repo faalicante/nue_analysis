2938cdc36bb387f7f1fd48eeaa396e3b1368e8eb
Fabio <alicantef@gmail.com>
2024-11-14 15:14:38 +0100

    change bkg eval

--- tag/tag_shift.C
+++ tag/tag_shift.C
@@ -22,91 +22,43 @@ void setPath(int data, TString* path, int cell, int* nPlates) {
         *nPlates = 60;
     }
     else if (data==2) {
-        // *path = TString::Format("/Users/fabioali/cernbox/shift/%i", cell);
+        // *path = TString("/Users/fabioali/cernbox");
         *path = TString::Format("/eos/experiment/sndlhc/users/falicant/RUN1/b121/shift/%i", cell);
         *nPlates = 57;
     }
 }
 
-void eval_bkg(TH2F &h2, float *bkg) { //just one clear area
-  float x1 = 292000; //b11 293000 //b21 292000 //b31 292000 //b41 292000 //b51 294000
-  float y1 = 91500;  //b11 88000  //b21 91500  //b31 88000  //b41 92000  //b51 90000
-  float r1 = 300;
-
-  int xmi1 = (int)(x1-r1);
-  int xma1 = (int)(x1+r1);
-  int ymi1 = (int)(y1-r1);
-  int yma1 = (int)(y1+r1);
-
-  int xmax1 = h2.GetXaxis()->FindBin(xma1);
-  int xmin1 = h2.GetXaxis()->FindBin(xmi1);
-  int ymax1 = h2.GetYaxis()->FindBin(yma1);
-  int ymin1 = h2.GetYaxis()->FindBin(ymi1);
-
-  float bkg1 = h2.Integral(xmin1,xmax1,ymin1,ymax1)/(xmax1-xmin1+1)/(ymax1-ymin1+1);
-  *bkg = bkg1;
-  // printf("Average background %f\n", *bkg);
-}
-
-void eval_bkg(TH2F &h2, TEllipse &el, float *bkg) { //two control regions next to the circle
-  float x0 = el.GetX1();
-  float y0 = el.GetY1();
-  float r1 = el.GetR1();
-
-  float x1 = x0 - r1 * 1.2;
-  float x2 = x0 + r1 * 1.2;
-  
-  int xmi1 = (int)(x1-r1);
-  int xma1 = (int)(x1+r1);
-  int ymi1 = (int)(y0-r1);
-  int yma1 = (int)(y0+r1);
-
-  int xmax1 = h2.GetXaxis()->FindBin(xma1);
-  int xmin1 = h2.GetXaxis()->FindBin(xmi1);
-  int ymax1 = h2.GetYaxis()->FindBin(yma1);
-  int ymin1 = h2.GetYaxis()->FindBin(ymi1);
-
-  int xmi2 = (int)(x2-r1);
-  int xma2 = (int)(x2+r1);
-  int ymi2 = (int)(y0-r1);
-  int yma2 = (int)(y0+r1);
-
-  int xmax2 = h2.GetXaxis()->FindBin(xma2);
-  int xmin2 = h2.GetXaxis()->FindBin(xmi2);
-  int ymax2 = h2.GetYaxis()->FindBin(yma2);
-  int ymin2 = h2.GetYaxis()->FindBin(ymi2);
-
-  float bkg1 = h2.Integral(xmin1,xmax1,ymin1,ymax1)/(xmax1-xmin1+1)/(ymax1-ymin1+1);
-  float bkg2 = h2.Integral(xmin2,xmax2,ymin2,ymax2)/(xmax2-xmin2+1)/(ymax2-ymin2+1);
-  *bkg = 0.5 * (bkg1 + bkg2);
-  // printf("Average background %f\n", *bkg);
-}
-
-int getMax(TH2F &h2, TObjArray &peaks, TObjArray &txt) {
-  Int_t MaxBin = h2.GetMaximumBin();
+int getMax(TH2F &h2, TObjArray &peaks, TObjArray &txt, const int bkg) {
   int rankbin = h2.GetMaximum();
-  Int_t ix,iy,iz;
-  int radius = 300;
-  h2.GetBinXYZ(MaxBin, ix, iy, iz);
-  float x = ((TAxis*)h2.GetXaxis())->GetBinCenter(ix);
-  float y = ((TAxis*)h2.GetYaxis())->GetBinCenter(iy);
-  TEllipse  *el = new TEllipse(x,y,radius,radius);
-  el->SetFillStyle(0);
-  peaks.Add(el);
-  TText  *t = new TText(x,y+300,Form("%d",peaks.GetEntries()));
-  t->SetTextSize(0.03);
-  txt.Add(t);
-  int r0=6;
-  for(int iix = ix-r0; iix<=ix+r0; iix++) //to fix remove the circle not square
-    for(int iiy = iy-r0; iiy<=iy+r0; iiy++)
-      h2.SetBinContent(iix,iiy,0);
-  return rankbin;
+  if (rankbin > bkg) {
+    Int_t MaxBin = h2.GetMaximumBin();
+    Int_t ix,iy,iz;
+    int radius = 300;
+    h2.GetBinXYZ(MaxBin, ix, iy, iz);
+    float x = ((TAxis*)h2.GetXaxis())->GetBinCenter(ix);
+    float y = ((TAxis*)h2.GetYaxis())->GetBinCenter(iy);
+    TEllipse  *el = new TEllipse(x,y,radius,radius);
+    el->SetFillStyle(0);
+    peaks.Add(el);
+    TText  *t = new TText(x,y+300,Form("%d",peaks.GetEntries()));
+    t->SetTextSize(0.03);
+    txt.Add(t);
+    int r0=6;
+    for(int iix = ix-r0; iix<=ix+r0; iix++) {//to fix remove the circle not square
+      for(int iiy = iy-r0; iiy<=iy+r0; iiy++) {
+        float distance = (pow(iix-ix,2)+pow(iiy-iy,2))/pow(r0,2);
+        if (distance <= 1) h2.SetBinContent(iix,iiy,0);
+      }
+    }
+    return rankbin;
+  }
+  else return 0;
 }
 
-void get_peaks(TH2F &h2, TObjArray &peaks, TObjArray &txt, int npmax, int *ranks) {
+void get_peaks(TH2F &h2, TObjArray &peaks, TObjArray &txt, int npmax, int *ranks, const int bkg) {
   TH2F *h2new = (TH2F*)h2.Clone("get_peaks");
   for(int i=0; i<npmax; i++){
-    int rankbin = getMax(*h2new, peaks, txt);
+    int rankbin = getMax(*h2new, peaks, txt, bkg);
     ranks[i] = rankbin;
   }
 }
@@ -122,13 +74,12 @@ void drawEllipse(TObjArray &peaks, TObjArray &txt, int col) {
   }
 }
 
-void getEntriesInEllipse(TH2F &h2, TEllipse &el, int *signif_bins, float *entries, float *bkg) {
+void getEntriesInEllipse(TH2F &h2, TEllipse &el, float *entries, float *bkg) {
   int nBinsX = h2.GetNbinsX();
   int nBinsY = h2.GetNbinsY();
   float x0 = el.GetX1();
   float y0 = el.GetY1();
   float r1 = el.GetR1();
-  *signif_bins = 0;
   *entries = 0;
 
   for (int i = 1; i <= nBinsX; ++i) {
@@ -149,33 +100,29 @@ void getEntriesInEllipse(TH2F &h2, TEllipse &el, int *signif_bins, float *entrie
 
 void count_bins(int npmax, TH2F &h2, TObjArray &peaks, int plate, TH1F **h_long, int data) {
   int np = peaks.GetEntries();
-  int signif_bins;
   float bkg = 0;
   float entries;
 
   for(int i=0; i<np; i++) {
     TEllipse *el = ((TEllipse*)(peaks.At(i)));
-    eval_bkg(h2, *el, &bkg);
-    // if (data==0) eval_bkg(h2, &bkg);
-    // else if (data==2) eval_bkg(h2, *el, &bkg);
-    getEntriesInEllipse(h2, *el, &signif_bins, &entries, &bkg);
+    getEntriesInEllipse(h2, *el, &entries, &bkg);
     if (entries<0) entries = 0;
     h_long[i]->SetBinContent(plate, entries);
   }
 }
 
 void makePlots(int combination, TCanvas *c, int np, int npmax, TH1F *h_long, int *maxPeak, int *maxPlate) {
-  // int idx = (np%3) +1;
-  // if (idx==1) c->Clear("D");
-  // c->cd(idx)->SetGrid(1,0);
+  int idx = (np%3) +1;
+  if (idx==1) c->Clear("D");
+  c->cd(idx)->SetGrid(1,0);
   *maxPeak = h_long->GetMaximum(),
   *maxPlate = h_long->GetMaximumBin();
-  // h_long->SetLineColor(1);
-  // h_long->SetLineWidth(2);
-  // h_long->Draw("hist");
-  // if(np == 2)                 c->Print(Form("%s/tag/longitudinal_xz_%i.pdf(", path.Data(), combination), "pdf");
-  // else if(np == npmax-1)      c->Print(Form("%s/tag/longitudinal_xz_%i.pdf)", path.Data(), combination), "pdf");
-  // else if(idx == 3 && np >2 ) c->Print(Form("%s/tag/longitudinal_xz_%i.pdf", path.Data(), combination), "pdf");
+  h_long->SetLineColor(1);
+  h_long->SetLineWidth(2);
+  h_long->Draw("hist");
+  if(np == 2)                 c->Print(Form("%s/tag/longitudinal_xz_%i.pdf(", path.Data(), combination), "pdf");
+  else if(np == npmax-1)      c->Print(Form("%s/tag/longitudinal_xz_%i.pdf)", path.Data(), combination), "pdf");
+  else if(idx == 3 && np >2 ) c->Print(Form("%s/tag/longitudinal_xz_%i.pdf", path.Data(), combination), "pdf");
 }
 
 void findStart(TH1F* h_long, int *firstPlate, int *lastPlate) { //add second axis?
@@ -187,10 +134,9 @@ void findStart(TH1F* h_long, int *firstPlate, int *lastPlate) { //add second axi
 void makeNtuple(int combination, int np, TH1F **h_long, TObjArray &peaks, int *ranks) {
   TString outputFileName = TString::Format("%s/tag/peaks_shift_%d.root", path.Data(), combination);
   TFile *outputFile = new TFile(outputFileName, "RECREATE");
-  // TFile *output = new TFile(Form("%s/peaks_shift_%i.root", path.Data(), combination),"RECREATE");
   TNtuple *ntuple = new TNtuple("showers","tagged showers","combination:tag:x:y:start:end:peak:maxplate:nseg:rankbin");
-  // TCanvas *c2 = new TCanvas("c2", "c2", 1500, 1500);
-  // c2->Divide(1,3);
+  TCanvas *c2 = new TCanvas("c2", "c2", 1500, 1500);
+  c2->Divide(1,3);
   
   float entries;
   float width;
@@ -228,22 +174,25 @@ int main(int argc, char* argv[]) {
   TFile* f = TFile::Open(file);
 
   int ntag = 10;
+  const int bkg = 500;
   TH2F *h2 = (TH2F *)(f->Get("XYseg"));
   h2->Smooth();
   TCanvas *c = new TCanvas("c", "c", 800, 800);
-  // c->SetGrid();
+  c->SetGrid();
   gStyle->SetOptStat(0);
   h2->Draw("colz");
   c->Update();
-  // c->Print(Form("%s/tag/sh_%i.gif+180", path.Data(), combination));
-  
+  c->Print(Form("%s/tag/sh_%i.gif+180", path.Data(), combination));
   TObjArray peaks;
   TObjArray txt;
   int ranks[ntag];
-  get_peaks(*h2,peaks,txt,ntag,ranks);
+  get_peaks(*h2,peaks,txt,ntag,ranks,bkg);
+  h2->GetZaxis()->SetRangeUser(bkg, ranks[0]);
+  c->Update();
+  c->Print(Form("%s/tag/sh_%i.gif+180", path.Data(), combination));
   drawEllipse(peaks,txt, kBlack);
   c->Update();
-  // c->Print(Form("%s/tag/sh_%i.gif+180", path.Data(), combination));
+  c->Print(Form("%s/tag/sh_%i.gif+180", path.Data(), combination));
 
   
   TH1F *h_long[ntag];
@@ -261,9 +210,9 @@ int main(int argc, char* argv[]) {
     drawEllipse(peaks,txt, kBlack);
     count_bins(ntag, *h, peaks, p, &h_long[0], data);
     c->Update();
-    // c->Print(Form("%s/tag/sh_%i.gif+12", path.Data(), combination));
+    c->Print(Form("%s/tag/sh_%i.gif+12", path.Data(), combination));
   }
-  // c->Print(Form("%s/tag/sh_%i.gif++", path.Data(), combination));
+  c->Print(Form("%s/tag/sh_%i.gif++", path.Data(), combination));
 
   makeNtuple(combination, ntag, &h_long[0], peaks,ranks);
 
