aa033027193f477115a3767d3cbb96ebeb977bd0
Fabio <alicantef@gmail.com>
2025-12-10 16:36:37 +0100

    23 cm shift map

--- shift_map.py
+++ shift_map.py
@@ -1,45 +1,78 @@
 import ROOT
 import numpy as np
+from matplotlib.colors import LogNorm
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
 from skimage.feature import peak_local_max
 
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument("--cell",dest="cell", type=int, required=True)
+parser.add_argument("-d",dest="data", type=int, required=True)
+options = parser.parse_args()
+cell = options.cell
+data = options.data
 
 ROOT.gStyle.SetOptStat("e")
+map = {}
 
-def makeTMap(hTmap):
-    smoothed_hist = gaussian_filter(hTmap, sigma=1)
-    peaks = peak_local_max(smoothed_hist, min_distance=2, threshold_abs=100)
-    peak_x_coords = txedges[peaks[:, 0]] 
-    peak_y_coords = tyedges[peaks[:, 1]] 
+def makeTMap(hTmap, peaks):
+    smoothed_hist = gaussian_filter(hTmap, sigma=0)
+    # peaks = peak_local_max(smoothed_hist, min_distance=1, threshold_abs=100)
+    peak_x_coords = peaks[0]
+    peak_y_coords = peaks[1]
+    peak_tx_coords = []
+    peak_ty_coords = []
+    output = ROOT.TFile.Open(f'peak_map_{cell}.root', 'RECREATE')
+    outtree = ROOT.TNtuple("showers", "showers", "cell:combination:tag:tx:ty:x:y:p:peak")
+    for px, py in zip(peak_x_coords, peak_y_coords):
+        # print(px,py)
+        x = np.searchsorted(xedges, px)
+        y = np.searchsorted(yedges, py)
+        if (x,y) not in map: continue
+        combination = map[(x,y)][1]
+        ix = combination % nbins
+        iy = combination // nbins
+        #if abs(ix-25)==4 or abs(iy-25)==4:
+            #print(map[(x,y)], ix-25, iy-25, px, py)
+        outtree.Fill(map[(x,y)][0], map[(x,y)][1], map[(x,y)][2], ix*2-50, iy*2-50, px, py, map[(x,y)][3], map[(x,y)][4])
+        tx_coord = txedges[ix]
+        ty_coord = tyedges[iy]
+        peak_tx_coords.append(int(tx_coord)+1)
+        peak_ty_coords.append(int(ty_coord)+1)
     plt.figure(figsize=(8, 6))
-    plt.imshow(smoothed_hist.T, origin='lower',extent=[-shiftRange, shiftRange+shiftStep, -shiftRange, shiftRange+shiftStep],cmap='viridis', interpolation='nearest')
-    # plt.plot(peak_x_coords+ shiftStep/2, peak_y_coords+ shiftStep/2, 'r.', markersize=10, label='Peaks')
+    plt.imshow(smoothed_hist.T, origin='lower',extent=[-shiftRange, shiftRange+shiftStep, -shiftRange, shiftRange+shiftStep],cmap='viridis', interpolation='none')
+    plt.plot(peak_tx_coords, peak_ty_coords, 'r.', markersize=10, label='Peaks')
     plt.colorbar(label='Counts')
-    plt.title('Nue simulation')
+    plt.title('2D Histogram with Detected Peaks')
     plt.xlabel('TX')
     plt.ylabel('TY')
     plt.legend()
-    plt.savefig(f'/Users/fabioali/Desktop/shifts/shift_Tmap_nu_100.png')
+    # plt.show()
+    plt.savefig(f'shift_Tmap_{cell}.png')
     plt.close()
+    output.cd()
+    outtree.Write()
+    output.Close()
 
-    return peak_x_coords, peak_y_coords
+    return peak_tx_coords, peak_ty_coords
 
 def makeMap(hmap):
     smoothed_hist = gaussian_filter(hmap, sigma=1)
-    peaks = peak_local_max(smoothed_hist, min_distance=5, threshold_abs=100)
+    peaks = peak_local_max(smoothed_hist, min_distance=2, threshold_abs=0)
     peak_x_coords = xedges[peaks[:, 0]]
-    peak_y_coords = yedges[peaks[:, 1]] 
+    peak_y_coords = yedges[peaks[:, 1]]
     plt.figure(figsize=(8, 6))
-    plt.imshow(smoothed_hist.T, origin='lower',extent=[xOffset,xRange+xOffset,yOffset,xRange+yOffset],cmap='viridis', interpolation='nearest')
-    plt.plot(peak_x_coords+xStep/2, peak_y_coords+xStep/2, 'r.', markersize=1, label='Peaks')
+    plt.imshow(smoothed_hist.T, origin='lower',extent=[xCenter-xRange, xCenter+xRange, yCenter-xRange, yCenter+xRange],cmap='viridis', interpolation='none')
+    plt.plot(peak_x_coords+xStep/2, peak_y_coords+xStep/2, 'r.', markersize=5, label='Peaks')
     # plt.plot(xedg,yedg, 'r.', markersize=5, label='Peaks')
     plt.colorbar(label='Counts')
     plt.title('2D Histogram with Detected Peaks')
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.legend()
-    plt.savefig(f'/Users/fabioali/Desktop/shifts/shift_nue_100.png')
+    # plt.legend()
+    # plt.show()
+    plt.savefig(f'shift_map_{cell}.png')
     plt.close()
 
     return peak_x_coords, peak_y_coords
@@ -48,21 +81,39 @@ def makeMap(hmap):
 # Parameters
 shiftRange = 50  #mrad
 shiftStep  = 2   #mrad
-nbins      = int((shiftRange * 2)/shiftStep) + 1 
-xRange = 200000  #um
-xOffset = 0      #um
-yOffset = 0      #um
+nbins      = int((shiftRange * 2)/shiftStep) + 1
+xRange = 6000  #um
+if data==0:
+    xcell = cell%21
+    ycell = cell//21
+    xCenter = (xcell-0.5)*10000 + 200000
+    yCenter = (ycell-0.5)*10000 - 5000
+    dz = 1315
+elif data==1:
+    dz = 1315
+    with open ('nue_int_1.txt', 'r') as f:
+        lines = f.readlines()
+        line = lines[cell].strip().split(",")
+        xCenter = float(line[2])
+        yCenter = float(line[3])
+elif data==2:
+    dz = 1350
+    xcell = cell%18
+    ycell = cell//18
+    xCenter = (xcell+1)*10000
+    yCenter = (ycell+1)*10000
+# xOffset = 288000      #um
+# yOffset = 83000      #um
 xStep  = 150     #um
-nxbins      = int(xRange/xStep)
-nTag = 50
+nxbins = int(2*xRange/xStep)
+nTag = 30
 
 # File
-path = '/Users/fabioali/cernbox/peaks_shift_100tag.root'
+# path = f'/eos/experiment/sndlhc/users/falicant/RUN1/b121/shift/{cell}/peaks.root'
+# path = f'/Users/fabioali/cernbox/test_shift/trk/{cell}/peaks.root'
+path = f'peaks_{cell}.root'
 file = ROOT.TFile.Open(path)
 showers = file.Get("showers")
-# pathSim = '/Users/fabioali/cernbox/nue_showers.root'
-# fileSim = ROOT.TFile.Open(pathSim)
-# cbmsim = fileSim.Get("cbmsim")
 
 tx = np.zeros(nbins)
 ty = np.zeros(nbins)
@@ -71,45 +122,35 @@ y = np.zeros(nxbins)
 ex = np.zeros(nxbins)
 ey = np.zeros(nxbins)
 
-hmap, xedges, yedges = np.histogram2d(x, y, nxbins, range=[[xOffset,xRange+xOffset], [yOffset,xRange+yOffset]])
+
+hmap, xedges, yedges = np.histogram2d(x, y, nxbins, range=[[xCenter-xRange, xCenter+xRange], [yCenter-xRange, yCenter+xRange]])
 hTmap, txedges, tyedges = np.histogram2d(tx, ty, nbins, range=[[-shiftRange, shiftRange+shiftStep], [-shiftRange, shiftRange+shiftStep]])
 
 for entry in showers:
-    combination = int(entry.combination) - 1 
+    if entry.nfound > 1: continue
+    combination = int(entry.combination)
     peak = int(entry.rankbin)
     tag = int(entry.tag)
     if tag > nTag : continue
-    # if abs(entry.x-77000)>5000 or abs(entry.y-91000)>5000: continue
-    x = (int(entry.x) - xOffset) // xStep
-    y = (int(entry.y) - yOffset) // xStep
-    ix = combination // nbins
-    iy = combination % nbins
-    # if ROOT.TMath.Sqrt((ix-25)**2+(iy-25)**2)<5: continue
-    if peak > hmap[x,y]:
-        hmap[x,y]= peak
+    # if peak<500: continue
+    ix = combination % nbins
+    iy = combination // nbins
+    tx = ix*2-50
+    ty = iy*2-50
+    plate = entry.maxplate
+    projx = entry.x - tx/1000 * ((plate-1) * dz)
+    projy = entry.y - ty/1000 * ((plate-1) * dz)
+    if abs(projx-xCenter)>5000 or abs(projy-yCenter)>5000: continue
+    x = int((projx - xCenter + xRange) // xStep)
+    y = int((projy - yCenter + xRange) // xStep)
+    # if abs(x-2)<3 or abs(x-nxbins+2)<3 or abs(y-2)<3 or abs(y-nxbins+2)<3 : continue
+    # if ROOT.TMath.Sqrt((ix-25)**2+(iy-25)**2)<7: continue
+    # if combination != 1234: continue
     if peak > hTmap[ix, iy]:
         hTmap[ix, iy] = peak
+    if peak > hmap[x,y]:
+        hmap[x,y] = peak
+        map[(x, y)] = (entry.cell, combination, tag, entry.start, peak)
 
-xedg = np.linspace(400, 0, 200000)
-yedg = np.linspace(400, 0, 200000)
-xedg = []
-yedg = []
-# "brick==22 && abs(ex+36.75)<9.25 && abs(ey-25.25)<9.25"
-# for entry in cbmsim:
-#     if entry.brick!=22:continue
-#     ex = entry.ex
-#     ey = entry.ey
-#     if abs(ex+36.75)<9.25 and abs(ey-25.25)<9.25:
-#         x = (ex+47.25) * 10000
-#         y = (ey-15.75) * 10000
-#         xedg.append(x)
-#         yedg.append(y)
-        
-
-# for tag in range(1, nTag+1):
-    # peaksT = makeTMap(hTmap[tag], tag)
-peaksT = makeTMap(hTmap)
 peaks = makeMap(hmap)
-print(len(peaksT[0]))
-
-#Brick_22: z=  307.7377cm  dZ=    3.8947cm  [  303.8430     311.6324] dx=    9.6498cm [  -45.9031     -26.6035] dy=    9.6490cm [   14.9472      34.2453]
+peaksT = makeTMap(hTmap, peaks)
